76a2b38258e0cfc81717825e55385375c5e5b912
Jeffwalrus <44712173+Jeffwalrus@users.noreply.github.com>
2019-01-02 15:21:33 -0500

    Add files via upload

---
+++ UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    int currentAmmo = 28;
+    int maxAmmo = 30;
+
+    void Update()
+    {
+        // Reload with R
+        if (Input.GetKeyDown("r"))
+        {
+            if (currentAmmo != maxAmmo)
+                currentAmmo = maxAmmo;
+        }
+        // If no Bullets, can´t shoot
+        if (currentAmmo != 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+                currentAmmo--;
+        }
+    }
+    // This is your ammo counter
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 150, 50), "Ammo: " + currentAmmo + " /  " + maxAmmo);
+    }
+}
